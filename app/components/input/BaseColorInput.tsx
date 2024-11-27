4fe610c0717812d385a27cd3be39efbb5b316f29
Nouman64-cat <noumanejazghost@gmail.com>
2024-11-27 22:13:31 +0500

    build bug remove

--- app/components/input/BaseColorInput.tsx
+++ app/components/input/BaseColorInput.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Wheel } from "@uiw/react-color";
+import { HexColorPicker } from "react-colorful";
 
 interface BaseColorInputProps {
   baseColor: string;
@@ -8,25 +8,21 @@ interface BaseColorInputProps {
   onColorChange: () => void;
 }
 
-const BaseColorInput: React.FC<BaseColorInputProps> = ({
-  baseColor,
-  setBaseColor,
-  onColorChange,
-}) => {
-  const handleColorChange = (color: any) => {
-    const newColor = color.hex; // Get the selected color in HEX format
-    setBaseColor(newColor); // Update the base color
+const BaseColorInput: React.FC<BaseColorInputProps> = ({ baseColor, setBaseColor, onColorChange }) => {
+  const handleColorChange = (color: string) => {
+    setBaseColor(color); // Update base color
     onColorChange(); // Trigger palette generation
   };
 
   return (
     <div className="flex flex-col items-center gap-4 font-poppins">
       {/* Color Wheel Picker */}
-      <Wheel
+      <HexColorPicker
         color={baseColor}
         onChange={handleColorChange}
+        className="w-80 h-80 rounded-full"
+        
       />
-
       <p className="text-sm font-poppins text-zinc-600">
         Selected Color: <span style={{ color: baseColor }}>{baseColor}</span>
       </p>
